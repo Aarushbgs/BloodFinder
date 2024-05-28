08537ecd45516316933059d4a3c92a14a402acdc
Aarush <aarushbegusarai@gmail.com>
2024-05-28 10:12:48 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Guidline from './Components/Guidline/guidline';
+import Footer from './Components/Footer/Footer';
+import Form from './Components/Form/Form';
+import MyForm from './Components/Form/MyForm';
+import Tips from './Components/Tips/Tips';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+          <Navbar />
+        
+          <Routes>
+          <Route path="/donor" element={<Form />} />
+            <Route path="/reciever" element={<MyForm />} />
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<Guidline />}/>
+            <Route path="/tips" element={<Tips/>}/>
+             
+          </Routes>
+         
+            
+        </div>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
